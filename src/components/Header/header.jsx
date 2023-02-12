76654bf2a62dcb139c6fb0bfd64788922868d2be
Az-amat <az-amat@rambler.ru>
2023-02-12 17:26:49 +0500

    dogfood_dz

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,21 @@
+import s from './index.module.css';
+import cn from 'classnames';
+
+
+function Header({children, user, onUpdateUser}) {
+
+  return (
+    <header className={cn(s.header,'cover')}>
+      <div className="container">
+        {user?.email && <span>{user?.email}</span>}
+        {user?.name && <span>{user?.name}</span>}
+
+        <div className={s.wrapper}>
+          {children}
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
